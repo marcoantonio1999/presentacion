1f3cd8a08e586cf9d75a6a08da978b61351b9f11
marcoantonio1999 <marcoorduna1999@ciencias.unam.mx>
2026-04-16 11:26:55 -0600

    Publish GHAS dashboard and updated React build

---
+++ react/assets/user-BfgFoRQb.js
@@ -0,0 +1,1 @@
+import{n as e}from"./jsx-runtime-BJfjsNBJ.js";var t=e(`trending-up`,[[`path`,{d:`M16 7h6v6`,key:`box55l`}],[`path`,{d:`m22 7-8.5 8.5-5-5L2 17`,key:`1t1m79`}]]),n=e(`user`,[[`path`,{d:`M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2`,key:`975kel`}],[`circle`,{cx:`12`,cy:`7`,r:`4`,key:`17ys0d`}]]);export{t as n,n as t};
